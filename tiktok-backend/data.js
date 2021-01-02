50e1431670f3c42bad9e3f09985653074afc05ab
sevgikaraagac <57255817+sevgikaraagac@users.noreply.github.com>
2021-01-02 18:08:34 +0300

    initial commit

---
+++ tiktok-backend/data.js
@@ -0,0 +1,31 @@
+const data = [
+    {
+        url: "https://ak.picdn.net/shutterstock/videos/32420230/preview/stock-footage-eating-and-leisure-concept-group-of-people-having-dinner-at-table-with-food.webm",
+        channel: "reactprogrammer",
+        description: "MERN STACK TİK TOK",
+        song: "99 problems but REACT aint one!",
+        likes: "111",
+        messages: "80",
+        shares: "12"
+    },
+    {
+        url: "https://ak.picdn.net/shutterstock/videos/32420230/preview/stock-footage-eating-and-leisure-concept-group-of-people-having-dinner-at-table-with-food.webm",
+        channel: "reactprogrammer",
+        description: "driving",
+        song: "500 miles",
+        likes: "341",
+        messages: "45",
+        shares: "6"
+    },
+    {
+        url: "https://ak.picdn.net/shutterstock/videos/32420230/preview/stock-footage-eating-and-leisure-concept-group-of-people-having-dinner-at-table-with-food.webm",
+        channel: "reactprogrammer",
+        description: "work from home",
+        song: "work work work!",
+        likes: "567",
+        messages: "40",
+        shares: "54"
+    }
+];
+
+export default data;
